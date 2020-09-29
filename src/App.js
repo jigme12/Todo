6bad7c6a5c076e36d11cc2b603ed290ccccef0db
Your Name <you@example.com>
2020-09-29 14:39:14 +0545

    route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
 import './App.css';
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
 import {v4} from 'uuid';
-
+import About from './components/pages/About'
 
 class App extends Component {
   state={
@@ -52,10 +53,17 @@ class App extends Component {
 
   render(){
   return (
+    <Router>
     <div className="App">
-      <div className='container'><Header/><AddTodo  addTodo={this.addTodo}/>
-      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/></div>
+      <div className='container'><Header/>
+      <Route exact path='/' render ={props=>(<React.Fragment>
+        <AddTodo  addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </React.Fragment>)}/>
+      <Route path='/about' component={About}/>
+     </div>
     </div> 
+    </Router>
   );
 }
 }
